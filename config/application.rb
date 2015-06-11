030574883b23237019d1782661a4c64bc161f829
David Heath and Tekin Suleyman <pairing+heathd+tekin@digital.cabinet-office.gov.uk>
2015-06-11 10:18:35 +0100

    Setup Proxy middleware in initializer
    Running `bundle exec rake` was failing because the environment variable
    GOVUK_UPSTREAM_URI was being set in rails_helper.rb, and this is loaded after
    applicaiton.rb. By moving the middleware setup to an initializer we can ensure
    that the environment variable is set in time.

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,6 @@ module AuthenticatingProxy
   class Application < Rails::Application
     config.autoload_paths += %W(#{config.root}/lib)
 
-    config.middleware.use "Proxy", ENV.fetch('GOVUK_UPSTREAM_URI')
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
