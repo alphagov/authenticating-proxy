a77bc41e85b47bf4cf85f659dee8a394b5df9de1
Tekin Suleyman <tekin@tekin.co.uk>
2015-06-12 10:13:09 +0100

    Generate coverage report using simplecov

--- Gemfile
+++ Gemfile
@@ -8,5 +8,6 @@ group :development, :test do
   gem 'rack-test', '0.6.3'
   gem 'webmock', '1.21.0', require: false
   gem 'byebug'
+  gem 'simplecov', '0.8.2', :require => false
+  gem 'simplecov-rcov', '0.2.3', :require => false
 end
-
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,10 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'rack/test'
 require 'webmock/rspec'
+require 'simplecov'
+require 'simplecov-rcov'
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+SimpleCov.start 'rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
