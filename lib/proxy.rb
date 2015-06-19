c0a4d1c031efbe78fa928b3824a087d7e0044309
David Heath and Tekin Suleyman <pairing+heathd+tekin@digital.cabinet-office.gov.uk>
2015-06-19 16:27:26 +0100

    Add some debug logging to Proxy class

--- lib/proxy.rb
+++ lib/proxy.rb
@@ -9,7 +9,13 @@ class Proxy < Rack::Proxy
   end
 
   def call(env)
-    (proxy?(env) && super) || @app.call(env)
+    if proxy?(env)
+      debug_logging(env, "Proxing request: #{env['PATH_INFO']}")
+      super
+    else
+      debug_logging(env, "Request not being proxied: #{env['PATH_INFO']}")
+      @app.call(env)
+    end
   end
 
   def proxy?(env)
@@ -31,5 +37,11 @@ class Proxy < Rack::Proxy
       headers.reject { |key, _| %w(status transfer-encoding).include?(key) },
       body
     ]
+  end
+
+private
+
+  def debug_logging(env, message)
+    env['action_dispatch.logger'] and env['action_dispatch.logger'].debug message
   end
 end
