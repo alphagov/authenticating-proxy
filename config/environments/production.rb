901e6a1c08e724737daffc084337f273d3d7a2ff
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-10-27 15:03:14 +0100

    Log to STDOUT and STDERR in production mode.
    This will allow this app to be monitored by Kibana (currently has a JSON
    error for each request)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,22 +71,19 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  config.logstasher.enabled = true
-  config.logstasher.logger = Logger.new(Rails.root.join("log/#{Rails.env}.json.log"))
-  config.logstasher.suppress_app_log = true
-
   # Use default logging formatter so that PID and timestamp are not suppressed.
   # config.log_formatter = ::Logger::Formatter.new
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+  config.logger = ActiveSupport::TaggedLogging.new(Logger.new($stderr))
 
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger = ActiveSupport::TaggedLogging.new(logger)
-  end
+  $real_stdout = $stdout.clone
+  $stdout.reopen($stderr)
+  config.logstasher.enabled = true
+  config.logstasher.logger = Logger.new($real_stdout)
+  config.logstasher.suppress_app_log = true
 
   # Do not dump schema after migrations.
   # config.active_record.dump_schema_after_migration = false
