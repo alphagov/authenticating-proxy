3932e23259704f06bff5f0dd29195d369cb076c3
Ben <benbaumann95@hotmail.co.uk>
2020-07-09 14:17:04 +0100

    Add authenticating-proxy to config.hosts This is required in order to make authenticating-proxy work with govuk-docker.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.hosts += [
+    "authenticating-proxy.dev.gov.uk",
+  ]
 end
