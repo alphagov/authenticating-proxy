7457ced6c3225fda9ead7a2a0a342ca4610e38fb
Ben <benbaumann95@hotmail.co.uk>
2020-08-27 14:08:50 +0100

    Remove unnecessary config from application.rb
    * The to_time setting refers to changes during the Ruby 2.3 to 2.4 migration.
    
    Remove unused require statements added by the rails upgrade

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,8 @@
 require_relative 'boot'
 
 require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
+
 require "action_controller/railtie"
-require "action_view/railtie"
-require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -15,16 +11,6 @@ Bundler.require(*Rails.groups)
 module AuthenticatingProxy
   class Application < Rails::Application
     config.load_defaults 6.0
-
-    # Enable per-form CSRF tokens. Previous versions had false.
-    config.action_controller.per_form_csrf_tokens = false
-
-    # Enable origin-checking CSRF mitigation. Previous versions had false.
-    config.action_controller.forgery_protection_origin_check = false
-
-    # Make Ruby 2.4 preserve the timezone of the receiver when calling `to_time`.
-    # Previous versions had false.
-    ActiveSupport.to_time_preserves_timezone = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
