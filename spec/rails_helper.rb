269a5c338ea99a86ec1f904d27b17d9e891123c4
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-09-08 17:36:29 +0100

    Fix incorrect coverage report
    Previously this wasn't picking up code being covered. This also
    removes the unnecessary RCov formatter.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,16 +1,17 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 ENV['GOVUK_UPSTREAM_URI'] = 'http://upstream-host.com'
+
+# Must be at top of file
+require 'simplecov'
+SimpleCov.start 'rails'
+
 require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'rack/test'
 require 'webmock/rspec'
-require 'simplecov'
-require 'simplecov-rcov'
-SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
-SimpleCov.start 'rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
