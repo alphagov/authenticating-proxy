a387e15aec9acedd65ad0e929de29bd8c0ba3021
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2020-11-12 09:25:26 +0000

    Improve healthcheck using govuk_app_config
    It ensures that the app is able to talk to the Mongo database. This also
    makes it closer to enabling CD for this app.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get "/healthcheck", to: proc { [200, {}, %w[OK]] }
+  get "/healthcheck", to: GovukHealthcheck.rack_response(
+    GovukHealthcheck::Mongoid,
+  )
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
