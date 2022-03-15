ad01e1c5d6703e35b591bb349a69bdd0e4f4d5f2
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2022-03-15 12:28:55 +0000

    Enable new Rails 7 configuration
    (One change - allow open redirects, necessary for SSO)

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,8 @@ Bundler.require(*Rails.groups)
 module AuthenticatingProxy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
+    Rails.application.config.action_controller.raise_on_open_redirects = false
 
     # Configuration for the application, engines, and railties goes here.
     #
