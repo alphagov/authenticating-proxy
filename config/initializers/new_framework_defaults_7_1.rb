33ea3b34d31d5aa1131a2a270f82a6f0d3125de9
Chris Roos <chris@seagul.co.uk>
2023-10-20 15:08:32 +0100

    Use Rails 7.1 run_after_transaction_callbacks_in_order_defined
    https://guides.rubyonrails.org/configuring.html#config-active-record-run-after-transaction-callbacks-in-order-defined
    
    We only have a single ActiveRecord model (`User`) and it doesn't define
    any callbacks so I'm happy that accepting this new default won't change
    any behaviour.

--- config/initializers/new_framework_defaults_7_1.rb
+++ config/initializers/new_framework_defaults_7_1.rb
@@ -168,7 +168,7 @@ Rails.application.config.active_record.marshalling_format_version = 7.1
 # Run `after_commit` and `after_*_commit` callbacks in the order they are defined in a model.
 # This matches the behaviour of all other callbacks.
 # In previous versions of Rails, they ran in the inverse order.
-# Rails.application.config.active_record.run_after_transaction_callbacks_in_order_defined = true
+Rails.application.config.active_record.run_after_transaction_callbacks_in_order_defined = true
 
 # Whether a `transaction` block is committed or rolled back when exited via `return`, `break` or `throw`.
 #
