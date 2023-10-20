07207bae536c98878dab89e4ae0fad4dbbfd8108
Chris Roos <chris@seagul.co.uk>
2023-10-20 12:47:38 +0100

    Run rails app:update using Rails 7.0.8
    By setting the Rails version to 7.0.8 in the Gemfile and then running
    `rails app:update`.
    
    I'm in the process of doing the same `rails app:update` dance for Rails
    7.1 and noticed that some of the changes it was making had already been
    introduced in Rails 7.0.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,10 +44,6 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
-  # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "authenticating_proxy_production"
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
@@ -67,4 +63,7 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
+
+  # Do not dump schema after migrations.
+  config.active_record.dump_schema_after_migration = false
 end
