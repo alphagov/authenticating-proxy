9a90d15d4606971773b99608a1efe9480f656b62
David Mays <david.mays@digital.cabinet-office.gov.uk>
2024-08-28 15:51:42 +0100

    Actionlint and Ruby 3.3.4

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module AuthenticatingProxy
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    # config.govuk_time_zone = "London"
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
