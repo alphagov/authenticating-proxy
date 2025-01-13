837029d2c442560863a53f7d7668b4f03d7e8da5
JonathanHallam <jonathan.hallam@digital.cabinet-office.gov.uk>
2025-01-13 09:48:01 +0000

    Run app:update

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ require "action_controller/railtie"
 # require "action_mailer/railtie"
 # require "action_mailbox/engine"
 # require "action_text/engine"
-# require "action_view/railtie"
+require "action_view/railtie"
 # require "action_cable/engine"
 # require "rails/test_unit/railtie"
 
@@ -26,7 +26,7 @@ module AuthenticatingProxy
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    # config.autoload_lib(ignore: %w[assets tasks])
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
