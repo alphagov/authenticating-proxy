fe2c049f3d47e0b8b8fdb5f9e38cbc1c53991cce
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2026-04-07 20:44:25 +0100

    Run `rails app:update` to update to rails 8.1
    Changes generated by running `bundle exec rails app:update` with manual
    overrides.
    
    Discarded the following changes:
    - bin.ci and config/ci.rb: we are currently using rake tasks in local
      development that integrate with GitHub Actions CI
    - config/puma.rb: we are using GovukPuma from govuk_app_config
    - removal of config.hosts and ENV["GOVUK_UPSTREAM_URI"] in
      config/environments/development.rb
    - removal of `config.public_file_server.enabled = false` in
      config/environments/production.rb
    - removal of `ENV["GOVUK_UPSTREAM_URI"]` in config/environments/test.rb
    - `require "action_view/railtie"` in config/application.rb: not needed
      https://github.com/alphagov/authenticating-proxy/commit/352b319a20a8d9d41f5eab003a96d912d4ee86c6
    
    Updated:
    - config.silence_healthcheck_path in config/environments/production.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,7 +20,6 @@ Rails.application.configure do
   if Rails.root.join("tmp/caching-dev.txt").exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
-
     config.public_file_server.headers = { "cache-control" => "public, max-age=#{2.days.to_i}" }
   else
     config.action_controller.perform_caching = false
@@ -41,11 +40,14 @@ Rails.application.configure do
   # Append comments with runtime information tags to SQL queries in logs.
   config.active_record.query_log_tags_enabled = true
 
+  # Highlight code that triggered redirect in logs.
+  config.action_dispatch.verbose_redirect_logs = true
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
   # Annotate rendered view with file names.
-  # config.action_view.annotate_rendered_view_with_filenames = true
+  config.action_view.annotate_rendered_view_with_filenames = true
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
